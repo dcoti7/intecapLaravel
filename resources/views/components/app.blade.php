7548f2ed6f2855ac9dd844331b8147535864cdc5
Daniel Coti <danielcoti7@gmail.com>
2024-08-21 09:29:06 -0600

    footer comentado

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -36,12 +36,12 @@
     {{$slot}}
 
     <!-- Footer -->
-    <footer class="bg-dark text-white mt-5">
+    {{-- <footer class="bg-dark text-white mt-5">
       
       <div class="bg-dark text-center py-3">
           <p class="mb-0">&copy; Fullstack Todos los derechos reservados.</p>
       </div>
-  </footer>
+    </footer> --}}
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
